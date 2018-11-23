ec87e06e835bd5ae3cdcbc540659f145d78b01f1
Mironchikov Ilya <i.mironchikov@gmail.com>
2018-11-23 17:00:01 +0300

    post

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,9 @@
+class PagesController < ApplicationController
+  def about
+    @title = 'About'
+    @text = 'Welcome'
+  end
+  def contacts
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'posts#index', as: 'home'
   get 'welcome/index'
+  get 'about' => 'pages#about', as: 'about'
+  resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
