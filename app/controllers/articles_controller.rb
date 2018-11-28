6cb43a6a8af242e459c90d835eb605ad64d37ff9
Mironchikov Ilya <i.mironchikov@gmail.com>
2018-11-28 14:28:18 +0300

    articles partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,4 @@
 class ArticlesController < ApplicationController
-  
   def index
     @articles = Article.all
   end
@@ -15,9 +14,10 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = 'Article was successfully created'
+      flash[:success] = 'Article was successfully created'
       redirect_to @article
     else
+      # redirect_back(fallback_location: 'new')
       render 'new'
     end
   end
@@ -29,17 +29,18 @@ class ArticlesController < ApplicationController
   def update
     @article = Article.find(params[:id])
     if @article.update(article_params)
-      flash[:notice] = 'Article was successfully updated'
+      flash[:success] = 'Article was successfully updated'
       redirect_to @article
     else
-      render 'edit'
+      flash[:danger] = 'Article was not updated!'
+      redirect_to edit_article_path(@article)
     end
   end
 
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
-    flash[:notice] = 'Article was successfully deleted'
+    flash[:success] = 'Article was successfully deleted'
     redirect_to articles_path
   end
 
