f143d8f7b8e8d3eee4e7e24a1599d2e8151d4caf
Mironchikov Ilya <i.mironchikov@gmail.com>
2018-12-06 16:12:25 +0300

    restrict UI

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[edit update show]
+
   def index
     @users = User.paginate(page: params[:page], per_page: 3)
   end
@@ -7,11 +9,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = 'Your profile was updated!'
       redirect_to articles_path
@@ -31,11 +31,14 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
