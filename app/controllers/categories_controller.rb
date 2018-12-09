5011f1847d351778b8dccccf203384cea8f38e6b
Mironchikov Ilya <i.mironchikov@gmail.com>
2018-12-09 18:19:33 +0300

    integration tests for category controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
   def index
-
+    @categories = Category.all
   end
 
   def show
@@ -8,10 +8,22 @@ class CategoriesController < ApplicationController
   end
 
   def new
-
+    @category = Category.new
   end
 
   def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:success] = 'Category created'
+      redirect_to categories_path
+    else
+      render 'new'
+    end
+  end
 
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
