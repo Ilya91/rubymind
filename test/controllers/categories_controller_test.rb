fdeaef6589cce7026ad238b43f5ffcddf19c40e3
Mironchikov Ilya <i.mironchikov@gmail.com>
2018-12-10 15:47:13 +0300

    should redirect create when admin not logged in

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -3,6 +3,8 @@ require 'test_helper'
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
   def setup
     @category = Category.create(name: 'sports')
+    @user = User.create(username: 'John', email: 'john@gmail.com', password: '1234',
+                        admin: true)
   end
 
   test 'should get categories index' do
@@ -11,6 +13,7 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should get new' do
+    sign_in_as(@user, '1234')
     get new_category_path
     assert_response :success
   end
@@ -18,5 +21,12 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   test 'should get show' do
     get category_path(@category)
     assert_response :success
+  end
+
+  test 'should redirect create when admin not logged in' do
+    assert_no_difference 'Category.count' do
+      post categories_path, params: {category:{name: 'sports'}}
+    end
+    assert_redirected_to categories_path
   end
 end
